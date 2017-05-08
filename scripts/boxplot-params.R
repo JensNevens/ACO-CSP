09a79576b1d85a87a082707e4599c0008c72bc2f
Jens Nevens <jnevens@vub.ac.be>
2017-05-08 14:27:54 +0200

    Added R-scripts and instances files.

---
+++ scripts/boxplot-params.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# Boxplot from file
+
+# Replace here the name of your file
+file.name <- "./../out/data-ants.txt"
+
+# Read file
+all.data <- read.table(file=file.name, header=TRUE, sep=":") #Replace the separator for any of your preference
+trials <- all.data[,1]  # Remove this line of there are no trial/instances
+data      <- all.data[,-1, drop=FALSE] # Get the data
+
+#Plot details in the file boxplot.R
+source("R-scripts/boxplot.R")
+do.boxplot(data.matrix=data, plot.title="Number of ants", output="boxplot-ants.png")
+
+
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ACO-CSP
+//
+//  Created by Jens Nevens on 8/05/17.
+//  Copyright © 2017 Jens Nevens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
